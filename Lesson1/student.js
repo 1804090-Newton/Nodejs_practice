56eb7ff71e89cf89a29029b088d238b7ff6c82f3
1804090-Newton <u1804090@student.cuet.ac.bd>
2023-10-10 10:25:06 +0600

    first commit

---
+++ Lesson1/student.js
@@ -0,0 +1,18 @@
+const getName=()=>{
+    return "Al Jabir Newton";
+}
+
+const getAge=()=>{
+    return "24";
+}
+
+const cgpa=3.97;
+//exports.Name=getName;
+//exports.age=getAge;
+//exports.cgpa=cgpa;
+
+module.exports={
+    getName,
+    getAge,
+    cgpa
+}
